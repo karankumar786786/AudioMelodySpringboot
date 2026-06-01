e173919bca847909f0f37aa879fd855e4956cc0c
Karan <saw_ug_24@ece.nits.ac.in>
2026-06-01 19:55:54 +0530

    chore: externalize application configuration to environment variables and enable redis ssl, while cleaning up database seeder syntax

--- melody/src/main/java/me/one_org/melody/Configuration/DatabaseSeeder.java
+++ melody/src/main/java/me/one_org/melody/Configuration/DatabaseSeeder.java
@@ -23,7 +23,7 @@ public class DatabaseSeeder implements CommandLineRunner {
     public void run(String... args) throws Exception {
         if (adminId == null) {
             throw new Exception();
-        }
+        };
         if (!usersRepository.existsById(adminId)) {
             Users admin = Users.builder()
                     .id(adminId)
@@ -33,8 +33,8 @@ public class DatabaseSeeder implements CommandLineRunner {
                     .build();
             if (admin == null) {
                 throw new Exception();
-            }
+            };
             usersRepository.save(admin);
-        }
+        };
     }
 }
