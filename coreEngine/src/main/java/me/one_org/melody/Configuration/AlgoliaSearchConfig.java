1d791f0ef2cbeaf3d17b8630dbd1ffadec06c8dd
karankumar786786 <onestream6969@gmail.com>
2026-08-16 22:54:44 +0530

    add

--- coreEngine/src/main/java/me/one_org/melody/Configuration/AlgoliaSearchConfig.java
+++ coreEngine/src/main/java/me/one_org/melody/Configuration/AlgoliaSearchConfig.java
@@ -1,11 +1,10 @@
 package me.one_org.melody.Configuration;
 
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
 import com.algolia.api.SearchClient;
-
-import org.springframework.beans.factory.annotation.Value;
 
 @Configuration
 public class AlgoliaSearchConfig {
